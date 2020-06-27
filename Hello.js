7ff7d24e7ae3f1755a6a50978facefe051db7e69
ashishdutta007 <ashish.dutta050@gmail.com>
2020-06-27 15:05:44 +0530

    Initial commit

---
+++ Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NormalDiv = props => <div {...props} />
+
+const App = () => {
+  const [randomValue, setRandomValue] = React.useState(0);
+  return (
+    <React.Fragment>
+      {new Array(50).fill(null).map((__, i) => (
+        <NormalDiv key={i}>Hello World</NormalDiv>
+      ))}
+      <button onClick={() => setRandomValue(Math.random())}>Force Rerender</button>
+    </React.Fragment>
+  );
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# cssin-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/cssin-js)
